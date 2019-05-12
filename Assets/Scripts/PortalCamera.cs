b9217efa96a7cb779123d9fb27763203f1522312
Daniel Ilett <danielilett@gmail.com>
2019-05-12 18:16:14 +0100

    Seamless portal rendering complete.
    Rendering is not yet recursive. However, the camera position and rotations are as required, and the stencil works as intended.

--- Assets/Scripts/PortalCamera.cs
+++ Assets/Scripts/PortalCamera.cs
@@ -5,36 +5,29 @@ using UnityEngine;
 public class PortalCamera : MonoBehaviour
 {
     [SerializeField]
-    private Transform inPortal;
+    private Transform[] portals = new Transform[2];
 
     [SerializeField]
-    private Transform outPortal;
+    private Camera[] portalCameras = new Camera[2];
 
     [SerializeField]
-    private Camera inPortalCamera;
-
-    [SerializeField]
-    private Camera outPortalCamera;
+    private RenderTexture[] portalTextures = new RenderTexture[2];
 
     [SerializeField]
     private Material portalMaterial;
 
-    [SerializeField]
-    private RenderTexture inPortalTex;
-
-    [SerializeField]
-    private RenderTexture outPortalTex;
+    private const int iterations = 2;
     
     private void OnRenderImage(RenderTexture src, RenderTexture dst)
     {
-        RenderCamera(inPortal, outPortal, inPortalCamera);
-        RenderCamera(outPortal, inPortal, outPortalCamera);
+        RenderCamera(portals[0], portals[1], portalCameras[0]);
+        RenderCamera(portals[1], portals[0], portalCameras[1]);
 
-        portalMaterial.SetTexture("_PortalTex", inPortalTex);
+        portalMaterial.SetTexture("_PortalTex", portalTextures[0]);
         portalMaterial.SetInt("_MaskID", 1);
         Graphics.Blit(src, src, portalMaterial, 1);
 
-        portalMaterial.SetTexture("_PortalTex", outPortalTex);
+        portalMaterial.SetTexture("_PortalTex", portalTextures[1]);
         portalMaterial.SetInt("_MaskID", 2);
         Graphics.Blit(src, src, portalMaterial, 1);
 
@@ -44,10 +37,21 @@ public class PortalCamera : MonoBehaviour
     private void RenderCamera(Transform inPortal, Transform outPortal, Camera renderCamera)
     {
         Vector3 relativePos = inPortal.InverseTransformPoint(transform.position);
-        Vector3 inCamPos = outPortal.TransformPoint(relativePos);
+        relativePos.x *= -1;
+        relativePos.z *= -1;
+        renderCamera.transform.position = outPortal.TransformPoint(relativePos);
 
-        renderCamera.transform.position = inCamPos;
-        renderCamera.transform.rotation = transform.rotation * Quaternion.Inverse(inPortal.rotation) * outPortal.rotation;
+        Vector3 relativeUpDir = inPortal.InverseTransformDirection(transform.up);
+        Vector3 relativeForwardDir = inPortal.InverseTransformDirection(transform.forward);
+
+        Vector3 newUpDir = outPortal.TransformDirection(relativeUpDir);
+        Vector3 newForwardDir = outPortal.TransformDirection(relativeForwardDir);
+
+        Quaternion newLookRotation = Quaternion.LookRotation(newForwardDir, newUpDir);
+
+        renderCamera.transform.localRotation = Quaternion.Euler(0, 180, 0) * newLookRotation;
+        
+        //renderCamera.transform.rotation = transform.rotation * Quaternion.Inverse(inPortal.rotation) * outPortal.rotation;
 
         renderCamera.nearClipPlane = Vector3.Distance(transform.position, inPortal.position);
         renderCamera.Render();
