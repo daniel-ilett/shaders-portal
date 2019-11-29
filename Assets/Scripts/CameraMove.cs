26518c2be8b0fb7ddbf39929fa50ad948f276372
Daniel Ilett <danielilett@gmail.com>
2019-11-29 21:48:00 +0000

    Refactored player controllers.

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -5,35 +5,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class CameraMove : MonoBehaviour
 {
-    [SerializeField]
-    private CameraMode cameraMode = CameraMode.Free;
-
     private const float moveSpeed = 7.5f;
     private const float cameraSpeed = 3.0f;
 
     private Vector2 rotation = Vector2.zero;
     private Vector3 moveVector = Vector3.zero;
 
-    private float jump = 0.0f;
-
     private new Rigidbody rigidbody;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
-
-        switch (cameraMode)
-        {
-            case CameraMode.Free:
-                rigidbody.useGravity = false;
-                GetComponent<Collider>().material = null;
-                break;
-            case CameraMode.Gravity:
-                rigidbody.useGravity = true;
-                break;
-        }
-
-
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -48,48 +30,12 @@ public class CameraMove : MonoBehaviour
         float y = Input.GetAxis("Elevation");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = Vector3.zero;
-        float speed = moveSpeed;
-
-        switch (cameraMode)
-        {
-            case CameraMode.Free:
-                move = new Vector3(x, y, z);
-                speed = moveSpeed * (Input.GetButton("Jump") ? 2.5f : 1.0f);
-                break;
-            case CameraMode.Gravity:
-                move = new Vector3(x, y, z);
-                if (Input.GetButtonDown("Jump"))
-                {
-                    jump = 5.0f;
-                }
-                break;
-        }
-
-        moveVector = move * speed;
+        moveVector = new Vector3(x, y, z) * moveSpeed;
     }
 
     private void FixedUpdate()
     {
-        switch (cameraMode)
-        {
-            case CameraMode.Free:
-                rigidbody.velocity = transform.TransformDirection(moveVector);
-                break;
-            case CameraMode.Gravity:
-                Vector3 velocity = transform.TransformDirection(moveVector);
-                velocity.y = rigidbody.velocity.y;
-                rigidbody.velocity = velocity;
-
-                rigidbody.AddForce(0.0f, jump, 0.0f, ForceMode.Impulse);
-                jump = 0.0f;
-                break;
-        }
+        rigidbody.velocity = transform.TransformDirection(moveVector);
 
     }
-}
-
-enum CameraMode
-{
-    Free, Gravity
 }
